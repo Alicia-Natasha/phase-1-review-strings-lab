5f212aeb97786b5e6f1bb5d41a767a3c38b8366e
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-04 10:53:35 +0300

    phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Isagi Yoichi';
+
+const welcomeMessage = 'Welcome to Flatbook, Isagi Yoichi!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = "Welcome, Isagi!";
